77426ba59b05e7ec86e901ec31fad39d16a50776
Franklin Ferreira Rezende Neto <2frneto@gmail.com>
2022-11-21 13:11:50 -0400

    inicio do projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from "express";
 
+const app = express();
 
-const app = express()
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  return res.json({
+    message: "Hello World Kenzie",
+  });
+});
 
-export default app
+app.listen(3001, () => {
+  console.log("deu certo, mané");
+});
+
+export default app;
